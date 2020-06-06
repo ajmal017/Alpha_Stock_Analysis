5c9f6bea49e17358285310b8e123fd3cdaa92eac
Tianning Li <ltianningli@gmail.com>
2020-06-06 15:34:34 -0700

    update

--- plots/data_plot.py
+++ plots/data_plot.py
@@ -3,6 +3,7 @@ import matplotlib.dates as mdates
 from mpl_finance import candlestick_ohlc
 
 def line_plot(df, symbol, col='Close', figsize=(18,8)):
+    plt.figure(figsize=figsize)
     plt.plot('Datetime',col, data=df)
     plt.xlabel("Date")
     plt.ylabel("Price")
@@ -12,14 +13,10 @@ def line_plot(df, symbol, col='Close', figsize=(18,8)):
 
 def candlestick_plot(df, symbol, ohlc_col=['Datetime', 'Open', 'High', 'Low', 'Close'],figsize=(18,8)):
     ohlc_df = df[ohlc_col]
-    # Converting dates column to float values
     ohlc_df['Datetime'] = ohlc_df['Datetime'].map(mdates.date2num)
-    # Making plot
     fig, ax = plt.subplots(figsize=figsize)
-    # Converts raw mdate numbers to dates
     ax.xaxis_date()
     plt.xlabel("Date")
-    # Making candlestick plot
     candlestick_ohlc(ax, ohlc_df.values, width=0.4,
                      colorup='g', colordown='r', alpha=0.8)
     plt.ylabel("Price")
